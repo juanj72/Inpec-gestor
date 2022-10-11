1bd7f6ef1f09eea5d10bb452343b6d492bd649e5
juanj72 <juan.jara@campusucc.edu.co>
2022-10-11 16:01:38 -0500

    agregacion static nuevo

--- inpec/settings.py
+++ inpec/settings.py
@@ -28,7 +28,7 @@ DEBUG = True
 ALLOWED_HOSTS = ['inpecgestorintento57.azurewebsites.net']
 
 STATICFILES_DIRS=(
-    os.path.join(BASE_DIR,'inpec_gestor/gestor/static'),
+    os.path.join(BASE_DIR,'static'),
 )
 
 
@@ -134,4 +134,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "./static/")
+STATIC_ROOT = os.path.join(BASE_DIR, "/static/")
